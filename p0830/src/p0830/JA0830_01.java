c7548fc1a64e60d64d17b5affc539c2e623298c3
KOREAVC <KOREAVC@DESKTOP-IIDIC0K>
2023-08-30 18:12:17 +0900

    8춸30일 수업-변수

---
+++ p0830/src/p0830/JA0830_01.java
@@ -0,0 +1,15 @@
+package p0830;
+
+public class JA0830_01 {
+
+	public static void main(String[] args) {
+		//주석 : 설명문 - 화면에서 출력할때 println명령문을 씀
+		System.out.println("안녕하세요. 자바수업을 진행합니다");
+		System.out.println("로그인을 해주세요. ");
+		System.out.println("아이디 :");
+		System.out.println("비밀번호 :");
+	}
+	
+}
+
+	
---
+++ p0830/src/p0830/JA0830_02.java
@@ -0,0 +1,29 @@
+package p0830;
+
+public class JA0830_02 {
+
+	public static void main(String[] args) {
+		boolean bo = true;
+		boolean bo2 = false;
+		char ch = 'a';
+		char ch2 = 'b';
+		double d = 11.12;
+		byte by = 1;
+		short sh = 2;
+		int a = 1;
+		int in = 2100000000;
+		int b = 2;
+		long lo = 100000000000L;
+		float fl = 1.2f;
+		double dou = 1.2;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a % b);
+
+	}
+
+}
